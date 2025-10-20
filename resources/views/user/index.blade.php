eb7a6b3d8c8d451c70bf16d1c9933d0b487633df
selsamariva-byte <selsamariva@gmail.com>
2025-10-20 13:47:33 +0700

    praktikum4(jobseed5)clear

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -16,74 +16,64 @@
     @if (session('error'))
       <div class="alert alert-danger">{{ session('error') }}</div>
     @endif
-    <table class="table table-borehead table-striped table-hover table-sm" id="table_user">
+
+    <div class="row mb-3">
+      <div class="col-md-12">
+        <div class="form-group row">
+          <label class="col-1 control-label col-form-label">Filter:</label>
+          <div class="col-3">
+            <select class="form-control" id="level_id" name="level_id" required>
+              <option value="">- Semua -</option>
+              @foreach($level as $item)
+                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+              @endforeach
+            </select>
+            <small class="form-text text-muted">Level Pengguna</small> 
+          </div>
+        </div>
+      </div>
+    </div>  
+
+    <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
       <thead>
-        <tr><th>ID</th><th>username</th><th>Nama</th><th>Level pengguna</th><th>Aksi</th></tr>
-      </thead>
-    </table>
-  </div>
-
-
-  
         <tr>
           <th>No</th>
           <th>Username</th>
           <th>Nama</th>
-          <th>Level</th>
+          <th>Level Pengguna</th>
           <th>Aksi</th>
         </tr>
-    <div class="row">
-      <div class="col-md-12">
-      <div class="from-group row">
-        <label class="col-1 control-label col-form-label">Filter:</label>
-        <div class="col-3">
-          <select class ="form-control" id="level_id" name="level_id" required>
-            <option value="">- Semua -</option>
-            @foreach($level as $item)
-              <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
-            @endforeach
-          </select>
-          <small class="form-text text-muted">Level Pengguna</small> 
-        </div>
-      </div>
-      </div>
-    </div>  
-    <table class="table table-borehead table-striped table-hover table-sm" id="table_user">
-      <thead>
-        <tr><th>>No</th>
-          <th>Username</th>
-          <th>Nama</th>
-          <th>Level</th>
-          <th>Aksi</th></tr>
       </thead>
     </table>
-      
+  </div>
+</div>
 @endsection
 
 @push('js')
 <script>
 $(document).ready(function() {
-  $('#table_user').DataTable({
-      processing: true,
-      serverSide: true,
-      ajax: {
-          url: "{{ url('user/list') }}",
-          type: "POST", // sementara pakai GET (karena POST sempat HTML)
-          dataType: "json",
-          data: function (d) {
-              d.level_id = $('#level_id').val();
-          }
-      },
-      columns: [
-          { data: "DT_RowIndex", name: "DT_RowIndex", orderable: false, searchable: false, className: "text-center" },
-          { data: "username", name: "username" },
-          { data: "nama", name: "nama" },
-          { data: "level.level_nama", name: "level.level_nama" },
-          { data: "aksi", name: "aksi", orderable: false, searchable: false }
-      ]
+  var dataUser = $('#table_user').DataTable({
+    processing: true,
+    serverSide: true,
+    ajax: {
+      url: "{{ url('user/list') }}",
+      type: "POST",
+      dataType: "json",
+      data: function (d) {
+        d.level_id = $('#level_id').val();
+      }
+    },
+    columns: [
+      { data: "DT_RowIndex", name: "DT_RowIndex", orderable: false, searchable: false, className: "text-center" },
+      { data: "username", name: "username" },
+      { data: "nama", name: "nama" },
+      { data: "level.level_nama", name: "level.level_nama" },
+      { data: "aksi", name: "aksi", orderable: false, searchable: false }
+    ]
   });
-  $('#level_id').on('change',function(){
-      dataUser.ajax.reload();
+
+  $('#level_id').on('change', function() {
+    dataUser.ajax.reload();
   });
 });
 </script>
